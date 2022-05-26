cdee190d51a0d4598e5e0b6187f8898b0c0c9130
RajuSultan <rajusultan12@gmail.com>
2022-05-26 22:16:15 +0600

    edit

--- src/Component/Home/Banner/Banner.js
+++ src/Component/Home/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
                     <div>
                         <h1 className="text-5xl font-bold mb-5">WELLCOME...</h1>
                         <h1 className="text-4xl font-bold text-primary">Heavvy Metal</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        {/* <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
                         <Link to='/login'>
                             <button className="btn btn-primary">Get Started</button>
                         </Link>
